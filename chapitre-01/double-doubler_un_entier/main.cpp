23634045f5df3ef19d26278aabf36585bc6ea89f
Aimé Nasser <joram030309@gmail.com>
2026-09-18 17:42:03 -0700

    correction du type de donnees

--- chapitre-01/double-doubler_un_entier/main.cpp
+++ chapitre-01/double-doubler_un_entier/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main(){
-    float a,b;
+    int a,b;
     std::cout <<"entrer un nombre"<<std::endl;
     std::cin>> a;
-    float d=a*2;
+    int d=a*2;
     std::cout << d<<std::endl;
     return 0;
 }
