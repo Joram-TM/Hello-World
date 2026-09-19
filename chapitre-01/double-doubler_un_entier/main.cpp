2558f4fb89b19731b342cbe9c7f6098167d0f3cf
Aimé Nasser <joram030309@gmail.com>
2026-09-18 17:34:42 -0700

    prmier commit

---
+++ chapitre-01/double-doubler_un_entier/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    float a,b;
+    std::cout <<"entrer un nombre"<<std::endl;
+    std::cin>> a;
+    float d=a*2;
+    std::cout << d<<std::endl;
+    std::cin>>d;
+    return 0;
+}
